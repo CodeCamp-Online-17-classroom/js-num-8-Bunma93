15ed563aacaf2435f20a503fc8fc5f556f9fda87
Bunma93 <bunma93@gmail.com>
2024-07-08 13:05:17 +0700

    Js-num8

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 // write code here
+let radius = prompt("กรอกรัศมี(ซม.)");
+function CCF(r) {
+    const pie = 3.14;
+    let sum = 2*pie*r;
+    alert(`ความยาวเส้นรอบวง = ${sum.toFixed(2)} ซม.`);
+}
+
+CCF(radius);
